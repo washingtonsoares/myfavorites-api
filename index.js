0618fe7c85fff833caa897b4a0f945e36c0ee01a
Washington Soares <washingtonudi@gmail.com>
2018-01-12 14:03:53 -0200

    log

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const PORT = process.env.PORT || 3000
 app.get('/', (req, res)  => {
   const url = req.query.url
 
-  getOpenGraphData(url, response => res.send(response))
+  getOpenGraphData(url, response => {
+    console.log(`LOG: ${url}` )
+    res.send(response)
+  })
 })
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
